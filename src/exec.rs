f7928d27d67e0b8c146c677fee9342373a54ba98
Helly Guo <hellyguo@foxmail.com>
2025-08-08 00:43:50 +0800

    feat: print conflected files/dirs

--- src/exec.rs
+++ src/exec.rs
@@ -2,7 +2,7 @@ use crate::{
     fsio::list_current_dir,
     param::{ExecMode, Param},
 };
-use std::{fs, path::PathBuf};
+use std::{collections::HashMap, fs, path::PathBuf};
 
 pub(crate) fn execute(param: &Param) {
     let paths = list_current_dir(param);
@@ -19,6 +19,8 @@ fn print_replace_result(paths: Vec<PathBuf>, param: &Param) {
     }
     let mut output_vec = Vec::new();
     let from_regex = param.get_from_regex();
+    let mut existed = HashMap::new();
+    let mut conflect_count = 0;
     paths.iter().for_each(|x| {
         let from_target = x.file_name().unwrap().to_string_lossy().to_string();
         let to_result = from_regex
@@ -32,28 +34,42 @@ fn print_replace_result(paths: Vec<PathBuf>, param: &Param) {
                 &param.get_to_pattern(),
             )
             .to_string();
-        output_vec.push((from_target, to_result));
+        if existed.contains_key(&to_result) {
+            output_vec.push((from_target, to_result, String::from("duplicated")));
+            conflect_count += 1;
+        } else {
+            existed.insert(to_result.clone(), 0);
+            output_vec.push((from_target, to_result, String::from("OK")));
+        }
     });
     let max_left_len = output_vec.iter().fold(0, |acc, x| acc.max(x.0.len()));
     let max_right_len = output_vec.iter().fold(0, |acc, x| acc.max(x.1.len()));
-    let len = max_left_len + max_right_len + 3 + 4;
+    let max_status_len = output_vec.iter().fold(0, |acc, x| acc.max(x.2.len()));
+    // 4 = 4 * "|", 6 = 3 * 2 * ' '
+    let len = max_left_len + max_right_len + max_status_len + 4 + 6;
     let separator = vec!['-'; len].iter().collect::<String>();
     println!("{separator}");
     println!(
-        "| {} | {} |",
+        "| {} | {} | {} |",
         fill(&String::from("from"), max_left_len),
-        fill(&String::from("to"), max_right_len)
+        fill(&String::from("to"), max_right_len),
+        fill(&String::from("status"), max_status_len)
     );
     println!("{separator}");
     output_vec.iter().for_each(|x| {
         println!(
-            "| {} | {} |",
+            "| {} | {} | {} |",
             fill(&x.0, max_left_len),
-            fill(&x.1, max_right_len)
+            fill(&x.1, max_right_len),
+            fill(&x.2, max_status_len)
         )
     });
     println!("{separator}");
-    println!("This is dryrun. Execute with '-x' to execute.");
+    if conflect_count == 0 {
+        println!("This is dryrun. Execute with '-x' to execute.");
+    } else {
+        println!("Duplicate detected {conflect_count} files/dirs, please recheck.");
+    }
 }
 
 fn fill(source: &String, max: usize) -> String {
