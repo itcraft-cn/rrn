7c6286444875e25e5f9a7c5573c4f11678a4f3a6
Helly Guo <hellyguo@foxmail.com>
2025-08-21 16:27:38 +0800

    feat: setup locale

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use crate::{
     exec::execute,
     param::{print_help, ParseArgFailure},
 };
-use rust_i18n::{i18n, t};
+use rust_i18n::{available_locales, i18n, set_locale, t};
+use std::env;
 
-i18n!("locales", fallback = "en");
+i18n!("locales", fallback = "en, zh");
 
 fn main() {
+    setup_locale();
     let rs_param = param::args2param();
     if let Ok(param) = rs_param {
         execute(&param);
@@ -22,5 +24,16 @@ fn main() {
                 print_help();
             }
         }
+    }
+}
+
+fn setup_locale() {
+    let env_lang = env::var("LANG").unwrap();
+    let current_locale = env_lang.split_at(2).0;
+    let vec = available_locales!();
+    if vec.contains(&current_locale) {
+        set_locale(current_locale);
+    } else {
+        set_locale("en");
     }
 }
